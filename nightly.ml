dae798299172e0aa0faacca0754048c0b89dd38c
Fanuzei <103410944+Fanuzei@users.noreply.github.com>
2024-12-22 16:50:10 +0500

    Update nightly.ml

--- nightly.ml
+++ nightly.ml
@@ -1,1 +1,1 @@
-https://drive.usercontent.google.com/download?id=1TJ5C0mJRFrYqdatT6pB8q7gZEPgutmxT&export=download&authuser=0
+https://drive.usercontent.google.com/u/0/uc?id=13f_kDr4pC_2Py9Ga3YTIerjEYmqpSfSq&export=download
